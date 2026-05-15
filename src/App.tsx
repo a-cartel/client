d676dc5a74659dfa5bb4ccc6f5c07f5dc84c5b86
CalaMiTY0311 <axelsilas12@naver.com>
2026-05-15 15:08:59 +0900

    tailwind 경로 에러 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
-import Footer from './components/footer.tsx'
+import Footer from './components/Footer.tsx'
 import NavBar from './components/NavBar.tsx'
 
 export default function App() {
   return (
     <>
-    <NavBar />
-    <Footer />
-      
+      <NavBar />
+      <Footer />
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ import daisyui from 'daisyui'
 export default {
   content: [
     "./index.html",
-    "./src/*/.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", 
   ],
   plugins: [daisyui],
 }
